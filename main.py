90f14827728add0c226693b4d339262320281a7b
Filip <targosz.f@gmail.com>
2025-08-04 20:47:44 +0200

    dasd

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import socket
 
-
 HOST = '2.tcp.eu.ngrok.io'
 PORT = 18167              
 
-# 🔒 Dane logowania
-login = input("Login: ")
-password = input("Hasło: ")
-credentials = f"{login}:{password}"
-
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-        client_socket.connect((HOST, PORT))  # Połączenie z serwerem
+        client_socket.connect((HOST, PORT))
         print("🔗 Połączono z serwerem")
 
-        # 📤 Wysyłanie danych logowania
-        client_socket.sendall(credentials.encode())
+        message = input("Wpisz wiadomość do wysłania: ")
+        client_socket.sendall(message.encode())
 
-        # 📥 Odbieranie odpowiedzi
         response = client_socket.recv(1024)
         print("📨 Odpowiedź serwera:", response.decode())
 
 except Exception as e:
     print("❌ Wystąpił błąd:", e)
+
--- server.py
+++ server.py
@@ -23,7 +23,7 @@ def get_password_for_user(username, pasw):
         if result:
             return result[0]  == pasw
         else:
-            assert "użytkownik nie istnieje"
+            return False
 
     except Exception as e:
         print("Błąd:", e)
@@ -34,11 +34,77 @@ def get_password_for_user(username, pasw):
         if conn:
             conn.close()
 
+#New user
+def add_new_user(username, password):
+    conn = None
+    cur = None
+    try:
+        conn = psycopg2.connect(
+            dbname="B5Mlogin",
+            user="postgres",
+            password="haslo123",
+            host="localhost"
+        )
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO users (username, password) VALUES (%s, %s);",
+            (username, password)
+        )
+        conn.commit()  # zatwierdzamy zmiany w bazie
+        print(f"Użytkownik {username} dodany pomyślnie")
+        return True
+    except psycopg2.errors.UniqueViolation:
+        print(f"❌ Użytkownik {username} już istnieje")
+        return False
+    except Exception as e:
+        print("Błąd:", e)
+        return False
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+
+def loging(conn):
+    conn.sendall("Welcome in B5Message".encode())
+    conn.sendall("1. Log in".encode())
+    conn.sendall("2. Sign up".encode())
+
+    while True:
+        data = conn.recv(1024).decode().strip()
+        if data in ("1", "2"):
+            break
+        else:
+            conn.sendall("Wybierz 1 lub 2\n".encode())
+
+    #Logowanie
+    if data == "1":
+        while True:
+            conn.sendall("Login: ".encode())
+            login = conn.recv(1024).decode().strip()
+
+            conn.sendall("Password: ".encode())
+            password = conn.recv(1024).decode().strip()
+
+            if get_password_for_user(login, password):
+                conn.sendall("✅ Zalogowano pomyślnie\n".encode())
+                return login
+            else:
+                conn.sendall("Błędny login lub hasło, spróbuj jeszcze raz\n".encode())
+    
+    if data == "2":
+        conn.sendall("Login: ".encode())
+        login = conn.recv(1024).decode().strip()
+        conn.sendall("Password: ".encode())
+        password = conn.recv(1024).decode().strip()
+
+        add_new_user(login, password)
+        return login
+    
 def handle_client(conn, addr):
     with conn:
         print(f"📥 Połączono z {addr}")
-        data = conn.recv(1024).decode()
-        print(f"DEBUG: Odebrano dane: '{data}'")
+        loging(conn)
 
         try:
             login, password = data.split(":")
